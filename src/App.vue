e5d0491a7a4e1532eee1245a0f684b46d340c062
Malte Wirz <mail@maltewirz.com>
2019-12-09 09:12:00 +0100

    passing prop

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <Quote quote="A wonderful quote!"></Quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
 
     export default {
+        components: {
+            Quote
+        }
         
     }
 </script>
